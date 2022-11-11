9bafd8795b9ed9f7fc1bf1e8677887b3f70ffc31
kaviyarasumaran <kaviyarasumaran@gmail.com>
2022-11-11 12:48:28 -0500

    add row

--- lib/home/Course_page_body.dart
+++ lib/home/Course_page_body.dart
@@ -1,4 +1,10 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:course/utils/colors.dart';
+import 'package:course/widgets/big_text.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/small_text.dart';
 
 class CoursePageBody extends StatefulWidget {
   const CoursePageBody({Key? key}) : super(key: key);
@@ -8,9 +14,9 @@ class CoursePageBody extends StatefulWidget {
 }
 
 class _CoursePageBodyState extends State<CoursePageBody> {
-  @override
   Widget build(BuildContext context) {
     return Container(
+      color: Colors.redAccent,
       height: 320,
       child: PageView.builder(
           itemCount: 5,
@@ -21,14 +27,65 @@ class _CoursePageBodyState extends State<CoursePageBody> {
   }
 
   Widget _buildPageItem(int index) {
-    return Container(
-      height: 220,
-      margin: EdgeInsets.only(left: 5, right: 5),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30),
-          color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
-          image:
-              DecorationImage(image: AssetImage("assets/image/course1.png"))),
+    return Stack(
+      children: [
+        Container(
+          height: 220,
+          margin: EdgeInsets.only(left: 5, right: 5),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/image/course0.png"))),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 140,
+            margin: EdgeInsets.only(left: 20, right: 20, bottom: 15),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30), color: Colors.white),
+            child: Container(
+              padding: EdgeInsets.only(top: 10, left: 15, right: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BigText(text: "Full Stack Development"),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Wrap(
+                        children: List.generate(5, (index) {
+                          return Icon(
+                            Icons.star,
+                            color: AppColors.mainColor,
+                            size: 15,
+                          );
+                        }),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SmallText(text: "4.5"),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SmallText(text: "1287"),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SmallText(text: "comment")
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
